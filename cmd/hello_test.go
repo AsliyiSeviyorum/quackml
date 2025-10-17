29cb240b418c09ffe010c232e4f6950b0fb4290d
AsliyiSeviyorum <asliyicokseviyorm@gmail.com>
2025-10-17 19:11:04 +0300

    fix tests and add hello name update

--- cmd/hello_test.go
+++ cmd/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "hi"
+	got := Hello("Ahmet")
+	want := "hi, Ahmet"
 
 	if got != want {
 		t.Errorf("Expected: %q,  received: %q", want, got)
