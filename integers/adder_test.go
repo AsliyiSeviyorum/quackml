8fe1c24d3476e5cf211fbde27e3cb0521ad8e20d
AsliyiSeviyorum <asliyicokseviyorm@gmail.com>
2025-10-18 03:03:23 +0300

    Integers

---
+++ integers/adder_test.go
@@ -0,0 +1,21 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 3)
+	expected := 5
+
+	if sum != expected {
+		t.Errorf("Expected %d, got %d", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(3, 3)
+	fmt.Println(sum)
+	// Output: 66
+}
